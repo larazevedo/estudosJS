f135c7d73b223c46f183d3a755debc8c8020034d
Lara <lara.silvantos.azevedo@alumni.usp.br>
2020-04-25 20:16:15 -0300

    Iniciando com variaveis

---
+++ Capitulo01/variaveis.js
@@ -0,0 +1,19 @@
+let nome = "Super man"
+// Não se coloca aspas para números.
+let idade = 100
+let poder = "Super força"
+
+nome = "Batman"
+poder = "Rico"
+
+console.log(
+  `Nome: ${nome}
+   Idade: ${idade}
+   Poder: ${poder}
+  `
+)
+
+/* palavras chaves no JS que não se poded usar para criar variáveis:
+let class
+let function
+*/
