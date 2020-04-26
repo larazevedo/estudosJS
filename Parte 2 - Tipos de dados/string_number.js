cf16650b2a051880c9bb6b241fbb60b9dd894285
Lara <lara.silvantos.azevedo@alumni.usp.br>
2020-04-26 10:31:22 -0300

    string e number

---
+++ Parte 2 - Tipos de dados/string_number.js
@@ -0,0 +1,7 @@
+/* 
+String: um texto no JS             Number
+'meu poder'                        10
+"meu nome"                         0.4
+`meu nome`                         10.10
+"20"                               0.00001 
+*/
