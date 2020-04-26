f96cc848e84975ea0b2b2362630b284a277802b3
Lara <lara.silvantos.azevedo@alumni.usp.br>
2020-04-26 17:17:06 -0300

    tipos de operadores

---
+++ 02.Tipos de dados/operadores.js
@@ -0,0 +1,19 @@
+/* 
+      Operadores aritméticos 
+Adição: 1+1
+Subtração: 5-4
+Multiplicação: 2*4
+Divisão: 2/4
+Resto da divisão: 6/4 (sobra 2)
+Exponenciação: 2**4 (2x2x2x2)
+
+      Operadores de comparação
+Menor que: 1 < 2
+Maior que: 4 > 2
+Igual a: 1 === 1 (Boa prática: usar 3 sinais de =)
+Diferente de: 1 !== -2
+Menor ou igual que 10 <= 10
+Maior ou igual que 12 >= 9
+Ou: (idade < 18 ) || (idade > 25)
+E: (nome === "Lara") && (idade < 25)
+*/
