1d625b5f126745a8fcdcbbe8bc06da3aa2e9c56b
Lara <lara.silvantos.azevedo@alumni.usp.br>
2020-05-10 16:27:46 -0300

    array

---
+++ 02.Tipos de dados/arrays.js
@@ -0,0 +1,23 @@
+/*
+
+ÍNDICE | NOME
+  0      Bulbassaur
+  1      Pikachu
+  2      Squirtle
+  3      Charmander
+
+*/
+
+// Declaração:
+[
+  "Bulbassaur",
+  "Pikachu",
+  "Squirtle",
+  "Charmander",
+]
+
+[0] === "Bulbassaur",
+[1] === "Pikachu",
+[2] === "Squirtle",
+// Se a posição não existe, é undefined
+[5] === undefined
