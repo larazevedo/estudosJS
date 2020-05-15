a3cedebbc283509d9a4ea3382ea67854f885bc7f
Lara <lara.silvantos.azevedo@alumni.usp.br>
2020-05-14 22:32:15 -0300

    Manipulando objetos

--- 02.Tipos de dados/objetos.js
+++ 02.Tipos de dados/objetos.js
@@ -15,3 +15,37 @@ pessoa['nome'];
 // Criar uma nova propriedade para o objeto: 
 pessoa['nomeDaMae'] = 'Selma Maria'
 console.log(pessoa)
+
+const heroi = {
+    nome: 'Batman',
+    idade: 100,
+    sexo: 'Masculino'
+}
+
+console.log('nome', heroi.nome);
+console.log('idade', heroi['idade']);
+console.log('sexo', heroi.sexo);
+// Vem undefined
+console.log('naoExiste', heroi.naoExiste);
+
+// Adiciona um item no objeto
+heroi.id = 0001;
+console.log(heroi);
+
+// Pegando todas as chaves do objeto
+console.log(Object.keys(heroi));
+
+// Pegando todos os valores do objeto
+console.log(Object.values(heroi));
+
+// Juntando dois objetos
+const objeto1 = {
+  tamanho: '10 metros'
+}
+
+const novoObjeto = Object.assign(heroi, objeto1);
+console.log(novoObjeto);
+
+// Deleta chave do objeto
+delete novoObjeto.nome;
+console.log(novoObjeto);
